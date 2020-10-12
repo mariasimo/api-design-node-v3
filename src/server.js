f5e1da2ade9ceecc5012c35a1285fb2aed2cb9f9
María Simó <maria.simo@secuoyas.com>
2020-10-12 13:28:09 +0200

    Solved

--- src/server.js
+++ src/server.js
@@ -12,4 +12,13 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+app.get('/', (req, res) => res.send({ message: 'hello world' }))
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.send({ message: 'ok' })
+})
+
+export const start = () => {
+  app.listen(3000, () => console.log('Listening on port'))
+}
